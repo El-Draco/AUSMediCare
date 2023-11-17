cf5c5abf7a92fde128a6b4a200d7c2a1bebf07b3
Hiba Saleem <114260072+Hiba-Saleem@users.noreply.github.com>
2023-11-17 12:42:56 +0400

    Updated appointment package and added AppointmentManager

--- src/main/java/AppointmentManagement/Schedule.java
+++ src/main/java/AppointmentManagement/Schedule.java
@@ -1,7 +1,33 @@
 package AppointmentManagement;
+import DatabaseManagement.AppointmentsTableManager;
+import UserManagement.HealthCareOfficial;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import java.util.*;
 public class Schedule {
 
+    ArrayList<Appointment> appointments = new ArrayList<>();
+    ArrayList<HealthCareOfficial> doctors = new ArrayList<>();
+
+    boolean CheckAvailability(Date d, HealthCareOfficial doctor) {
+        boolean availdate = true, availdoctor = true;
+
+        for (Appointment appointment : appointments) {
+            if (appointment.getDate() == d) {
+                availdate = false;
+                break;
+            }
+        }
+
+        for (HealthCareOfficial official : doctors) {
+            if (Objects.equals(official.getAccount().getId(), doctor.getAccount().getId())) {
+                availdoctor = false;
+                break;
+            }
+        }
+        return availdoctor && availdate;
+    }
 }
+
